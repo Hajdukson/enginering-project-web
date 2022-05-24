426fc5e851952b24b21f9bcb811a92d8b54e1aed
Michał Hajduk <hajduk.michal1998@gmail.com>
2022-05-24 09:52:48 +0200

    Add DataContext

---
+++ MoneyManager/MoneyManager.DataAccess/Data/MoneyManagerDataContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MoneyManager.Models;
+
+namespace MoneyManager.DataAccess
+{
+    public class MoneyManagerDataContext : DbContext
+    {
+        public MoneyManagerDataContext (DbContextOptions<MoneyManagerDataContext> options)
+            : base(options)
+        {
+        }
+    }
+}
