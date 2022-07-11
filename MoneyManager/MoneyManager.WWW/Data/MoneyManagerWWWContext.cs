c62685f1f3e7818b9ea122037164d19c9b650117
Michał Hajduk <hajduk.michal1998@gmail.com>
2022-07-11 18:43:31 +0200

    Add Web Project

---
+++ MoneyManager/MoneyManager.WWW/Data/MoneyManagerWWWContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MoneyManager.WWW.Models;
+
+namespace MoneyManager.WWW.Data
+{
+    public class MoneyManagerWWWContext : DbContext
+    {
+        public MoneyManagerWWWContext (DbContextOptions<MoneyManagerWWWContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MoneyManager.WWW.Models.Category>? Category { get; set; }
+    }
+}
