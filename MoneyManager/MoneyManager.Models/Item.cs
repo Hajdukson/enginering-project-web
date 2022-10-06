05016e83446ee1fa8fd01eb721ce48dda1fb57b1
Michał Hajduk <hajduk.michal1998@gmail.com>
2022-10-06 17:56:14 +0200

    added Model validation

--- MoneyManager/MoneyManager.Models/Item.cs
+++ MoneyManager/MoneyManager.Models/Item.cs
@@ -10,11 +10,15 @@ namespace MoneyManager.Models
     {
         public int Id { get; set; }
         [Required]
+        [StringLength(60, MinimumLength = 3)]
         public string Name { get; set; }
         [Required]
+        [DataType(DataType.Date)]
         public DateTime TransactionDate { get; set; }
         [Required]
+        [DataType(DataType.Currency)]
         [Column(TypeName = "decimal(18,4)")]
+        [Range(0, double.MaxValue, ErrorMessage = "Value must be greater than 0")]
         public decimal Price { get ; set; } 
         [NotMapped]
         public virtual ItemType Type { get ; }
--- MoneyManager/MoneyManager.WWW/Controllers/UserManageController.cs
+++ MoneyManager/MoneyManager.WWW/Controllers/UserManageController.cs
@@ -1,27 +1,29 @@
-﻿using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
+﻿//FUTURE UPDATE
 
-namespace MoneyManager.WWW.Controllers
-{
-    [Route("api/[controller]")]
-    [ApiController]
-    public class UserManageController : Controller
-    {
-        public UserManager<IdentityUser> UserManager { get; private set; }
-        public ISecureDataFormat<AuthenticationTicket> AccessTokenFormat { get; private set; }
-        public UserManageController(UserManager<IdentityUser> userManager, ISecureDataFormat<AuthenticationTicket> accessTokenFormat)
-        {
-            UserManager = userManager;
-            AccessTokenFormat = accessTokenFormat;
-        }
+//using Microsoft.AspNetCore.Authentication;
+//using Microsoft.AspNetCore.Hosting;
+//using Microsoft.AspNetCore.Identity;
+//using Microsoft.AspNetCore.Mvc;
 
-        [HttpPost]
-        [Route("register")]
-        public IActionResult Register()
-        {
-            return View();  
-        }
-    }
-}
+//namespace MoneyManager.WWW.Controllers
+//{
+//    [Route("api/[controller]")]
+//    [ApiController]
+//    public class UserManageController : Controller
+//    {
+//        public UserManager<IdentityUser> UserManager { get; private set; }
+//        public ISecureDataFormat<AuthenticationTicket> AccessTokenFormat { get; private set; }
+//        public UserManageController(UserManager<IdentityUser> userManager, ISecureDataFormat<AuthenticationTicket> accessTokenFormat)
+//        {
+//            UserManager = userManager;
+//            AccessTokenFormat = accessTokenFormat;
+//        }
+
+//        [HttpPost]
+//        [Route("register")]
+//        public IActionResult Register()
+//        {
+//            return View();  
+//        }
+//    }
+//}
