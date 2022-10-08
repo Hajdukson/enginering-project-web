49c10ea711ac7ef13bacac8308955aea07f94794
Michał Hajduk <hajduk.michal1998@gmail.com>
2022-10-08 11:54:29 +0200

    added ClaimUserId Service

---
+++ MoneyManager/MoneyManager.Services/ClaimeUserId.cs
@@ -0,0 +1,16 @@
+﻿using MoneyManager.Services.Interfeces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyManager.Services
+{
+    public class ClaimeUserId : IClaimUserId
+    {
+        public string GetUserId(ClaimsIdentity identity) => identity.FindFirst(ClaimTypes.NameIdentifier).Value;
+    }
+}
---
+++ MoneyManager/MoneyManager.Services/Interfeces/IClaimeUserId.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyManager.Services.Interfeces
+{
+    //var claimsIdentity = (ClaimsIdentity)User.Identity;
+    //var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+    public interface IClaimUserId
+    {
+        string GetUserId(ClaimsIdentity identity);
+    }
+}
